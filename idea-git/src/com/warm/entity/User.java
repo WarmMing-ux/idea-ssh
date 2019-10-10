a036e29a0e32b95130f93d178060c13eaf8ca831
ming <quan17612047715@163.com>
2019-10-10 16:29:43 +0800

    在User类中添加name属性

--- idea-git/src/com/warm/entity/User.java
+++ idea-git/src/com/warm/entity/User.java
@@ -4,6 +4,16 @@ public class User {
 
     private Integer id;
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Integer getId() {
         return id;
     }
